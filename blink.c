edeec2fc6ee7dd2ebba085cec5adf0275dc7205f
Josh Headapohl <joshhead@gmail.com>
2013-05-26 14:35:46 -0400

    Use arrays

--- blink.c
+++ blink.c
@@ -26,55 +26,44 @@ void delay_ms(uint8_t ms) {
 
 int main(void) {
   uint8_t i;
-  uint8_t r_divider;
-  uint8_t g_divider;
-  uint8_t b_divider;
+  uint8_t which;
+  uint8_t leds[3];
+  uint8_t dividers[3];
 
-  r_divider = 130;
-  g_divider = 140;
-  b_divider = 150;
+  leds[0] = RED;
+  leds[1] = GREEN;
+  leds[2] = BLUE;
+
+  dividers[0] = 130;
+  dividers[1] = 140;
+  dividers[2] = 150;
 
   // initialize the direction of the B port to be outputs
   // on the 3 pins that have LEDs connected
-  set_output(DDRB, RED);
-  set_output(DDRB, GREEN);
-  set_output(DDRB, BLUE);
+  for (which = 0; which < 3; which++) {
+    set_output(DDRB, leds[which]);
+  }
 
   while (1) {
 
     for (i = 0; i < 255; i++) {
 
-      if (i % r_divider == 0) {
-        output_high(PORTB, RED);
-      } else {
-        output_low(PORTB, RED);
+      for (which = 0; which < 3; which++) {
+        if (i % dividers[which] == 0) {
+          output_high(PORTB, leds[which]);
+        } else {
+          output_low(PORTB, leds[which]);
+        }
       }
 
-      if (i % g_divider == 0) {
-        output_high(PORTB, GREEN);
-      } else {
-        output_low(PORTB, GREEN);
+    }
+
+    for (which = 0; which < 3; which++) {
+      dividers[which]++;
+
+      if (dividers[which] > 150) {
+        dividers[which] = 0;
       }
-
-      if (i % b_divider == 0) {
-        output_high(PORTB, BLUE);
-      } else {
-        output_low(PORTB, BLUE);
-      }
-    }
-
-    r_divider++;
-    g_divider++;
-    b_divider++;
-
-    if (r_divider > 150) {
-      r_divider = 0;
-    }
-    if (g_divider > 150) {
-      g_divider = 0;
-    }
-    if (b_divider > 150) {
-      b_divider = 0;
     }
   }
 }
